35346127513a4a6d803842fc7b4eceec792bffd3
mori1223 <68760197+mori211223@users.noreply.github.com>
2024-06-23 12:12:43 +0900

    Add files via upload

---
+++ aronanti/script.js
@@ -0,0 +1,18 @@
+{
+    const update = () => {
+      // 日本時間の timezone 付きで指定
+      const targetTime = new Date('2025-03-19T12:30+09:00').getTime();
+      const diff = targetTime - Date.now();
+      const numbers = [
+        diff / 1000 / 60 / 60 / 24,
+        (diff / 1000 / 60 / 60) % 24,
+        (diff / 1000 / 60) % 60,
+        (diff / 1000) % 60,
+      ];
+      Array.from(document.querySelectorAll('#container > .num'), (elm, i) => {
+        elm.textContent = `${Math.floor(numbers[i])}`.padStart(2, 0);
+      });
+    };
+    update();
+    setInterval(update, 1000);
+  }
